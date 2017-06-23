11dbb2ec60e32d595fd80945c739d7436f2fb736
Erwin Lutke <erwinlutke@gmail.com>
2017-06-23 07:57:49 +0200

    Added stage progression logic

--- WaterSmash/GameState/WorldMapGameState.cs
+++ WaterSmash/GameState/WorldMapGameState.cs
@@ -21,15 +21,15 @@ namespace Water
         KeyLocker keyLocker;
 
         /// <summary>
-        /// Holds how many stages are playable
-        /// NOT USED YET
+        /// Which stages are unlocked default is stage 1
         /// </summary>
         [DataMember]
-        int stageProgress { get; set; }
-       
+        int stageProgress = 1;
+
         /// <summary>
         /// Holds all data of the stages
         /// </summary>
+        [DataMember]
         List<StageData> stageData = new List<StageData>();
 
         /// <summary>
@@ -108,12 +108,13 @@ namespace Water
             }
             else if(gameStateManager.Previous is StageGameState)
             {
-                if(args.Length > 0)
+                if (args.Length > 0)
                 {
                     stageData[selectedStage - 1].record = (string)args[0];
+                    stageData[selectedStage - 1].difficulty++;
+                    stageProgress++;
                 }
             }
-
            
         }
 
@@ -150,6 +151,9 @@ namespace Water
             // We only want to draw a certain part of the worldmap depending on the zoom level (viewPortSize)
             Rectangle sourceRect = new Rectangle(currentLoc.X - (viewPortSize.X / 2), currentLoc.Y - (viewPortSize.Y / 2), viewPortSize.X, viewPortSize.Y);
             spriteBatch.Draw(images["worldmap"], graphicsDevice.Viewport.Bounds, sourceRect, Color.White);
+
+            if(stageProgress > 1) spriteBatch.Draw(images["road"], graphicsDevice.Viewport.Bounds, sourceRect, Color.White);
+
             spriteBatch.Draw(images["fog"], graphicsDevice.Viewport.Bounds, Color.White);
 
             // If we are on a selectable stage, draw the highlight of the stage
@@ -214,7 +218,7 @@ namespace Water
                     && currentLoc.Y > stage.startLocation.Y 
                     && currentLoc.Y < stage.endLocation.Y)
                 {
-                    return stage.level;
+                    if(stage.level <= stageProgress) return stage.level;
                 }
 
             }
@@ -227,6 +231,7 @@ namespace Water
         private void loadContent()
         {
             images.Add("worldmap", contentManager.Load<Texture2D>("Images/worldmap"));
+            images.Add("road", contentManager.Load<Texture2D>("Images/road"));
             images.Add("fog", contentManager.Load<Texture2D>("Images/fog"));
             images.Add("message", contentManager.Load<Texture2D>("Images/message"));
 
@@ -257,6 +262,7 @@ namespace Water
             stage_1.record = "--'--\"---";
             stage_1.startLocation = new Point(130, 255);
             stage_1.endLocation = new Point(190, 295);
+            stage_1.difficulty = 1;
 
             stage_2.level = 2;
             stage_2.name = "Cabin in the woods";
@@ -264,6 +270,7 @@ namespace Water
             stage_2.record = "--'--\"---";
             stage_2.startLocation = new Point(180, 175);
             stage_2.endLocation = new Point(240, 215);
+            stage_2.difficulty = 1;
 
             stageData.Add(stage_1);
             stageData.Add(stage_2);
@@ -275,6 +282,8 @@ namespace Water
         /// <param name="state">The current state of the keyboard</param>
         private void handleViewPortMovement(KeyboardState state)
         {
+            if (state.IsKeyDown(Keys.L)) stageProgress = 2;
+
             if (state.IsKeyDown(Keys.Up)) currentLoc.Y -= 1;
             else if (state.IsKeyDown(Keys.Down)) currentLoc.Y += 1;
             if (state.IsKeyDown(Keys.Left)) currentLoc.X -= 1;
@@ -349,6 +358,11 @@ namespace Water
         /// Which stage it is
         /// </summary>
         public int level;
+
+        /// <summary>
+        /// Will increase every time you beat the stage
+        /// </summary>
+        public int difficulty;
 
         /// <summary>
         /// Holds the highlight to render when the stage is hovered on
